1c24eed62bbb89acce77f64d68b0c0cfc8c060b1
chadmckellar <cdm090@usask.ca>
2023-08-20 18:35:20 -0600

    initial commit

---
+++ 242.valid-anagram.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+# https://leetcode.com/problems/valid-anagram/description/
+#
+# algorithms
+# Easy (63.18%)
+# Likes:    10168
+# Dislikes: 322
+# Total Accepted:    2.5M
+# Total Submissions: 3.9M
+# Testcase Example:  '"anagram"\n"nagaram"'
+#
+# Given two strings s and t, return true if t is an anagram of s, and false
+# otherwise.
+#
+# An Anagram is a word or phrase formed by rearranging the letters of a
+# different word or phrase, typically using all the original letters exactly
+# once.
+#
+#
+# Example 1:
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+# Example 2:
+# Input: s = "rat", t = "car"
+# Output: false
+#
+#
+# Constraints:
+#
+#
+# 1 <= s.length, t.length <= 5 * 10^4
+# s and t consist of lowercase English letters.
+#
+#
+#
+# Follow up: What if the inputs contain Unicode characters? How would you adapt
+# your solution to such a case?
+#
+#
+
+# @lc code=start
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+# @lc code=end
